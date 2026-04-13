4800edf949e2e707ef5869ef160f2551ca2f5d7a
bnaand <106130584+bnaand@users.noreply.github.com>
2026-04-13 22:03:12 +0800

    Update cudy-tr3000.sh

--- cudy-tr3000.sh
+++ cudy-tr3000.sh
@@ -21,8 +21,8 @@ rm -rf feeds/packages/net/{xray-core,v2ray-geodata,sing-box,chinadns-ng,dns2sock
 git clone https://github.com/xiaorouji/openwrt-passwall-packages package/passwall-packages
 
 rm -rf feeds/luci/applications/luci-app-passwall
-# git clone https://github.com/xiaorouji/openwrt-passwall package/passwall-luci
-git clone https://github.com/xiaorouji/openwrt-passwall2 package/passwall2-luci
+# git clone https://github.com/Openwrt-Passwall/openwrt-passwall package/passwall-luci
+git clone https://github.com/Openwrt-Passwall/openwrt-passwall2 package/passwall2-luci
 
 # tailscale
 # sed -i '/\/etc\/init\.d\/tailscale/d;/\/etc\/config\/tailscale/d;' feeds/packages/net/tailscale/Makefile
