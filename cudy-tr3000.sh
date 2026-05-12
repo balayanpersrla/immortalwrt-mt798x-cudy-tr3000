f56b7de1b7ce863223fdbbdc80c85dd5aa091e72
balayanpersrla <86528359+balayanpersrla@users.noreply.github.com>
2026-05-12 21:06:02 +0800

    Update cudy-tr3000.sh

--- cudy-tr3000.sh
+++ cudy-tr3000.sh
@@ -31,7 +31,7 @@ git clone https://github.com/Openwrt-Passwall/openwrt-passwall2 package/passwall
 # Modify default IP
 sed -i 's/192.168.6.1/192.168.10.1/g' package/base-files/files/bin/config_generate
 
-# set ubi to 122M
+set ubi to 122M
 sed -i 's/reg = <0x5c0000 0x7000000>;/reg = <0x5c0000 0x7a40000>;/' target/linux/mediatek/dts/mt7981b-cudy-tr3000-v1-ubootmod.dts
 
 # defconfig
